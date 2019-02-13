4c9725e8499434ede36f5a568b613e8fc8ff75c8
Nils Bore <nilsbore@gmail.com>
2019-02-13 17:58:50 +0100

    Simplified example

--- example/clock_streams.cpp
+++ example/clock_streams.cpp
@@ -18,28 +18,14 @@ int main(int argc, char** argv)
     streams.add_stream("Clock 4", 20, 50);
 
     size_t counter = 0;
-    bool ok = true;
-    while (ok) {
+    while (streams.spin()) {
         auto now = std::chrono::system_clock::now();
         auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
         std::stringstream ss;
         ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X") << ", " << counter;
-        //ss << in_time_t;
-
-        //cout << ss.str() << endl;
-
         streams[string("Clock ") + to_string(counter % 4 + 1)] << ss.str();
-        /*
-        streams["Clock1"] << "Clock 1";
-        streams["Clock2"] << "Clock 2";
-        streams["Clock3"] << "Clock 3";
-        streams["Clock4"] << "Clock 4";
-        */
-
         counter = (counter + 1) % 1000;
-
-        ok = streams.spin();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
